991a6ab9f4ba3ade2ff7318ac07730b03afd693a
Royce Branning <rbrannin@nd.edu>
2017-10-12 13:52:44 -0400

    formatted into project specifications

---
+++ server/utils.cpp
@@ -0,0 +1,102 @@
+/**
+ * src/utils.cpp
+ *
+ * Implementation of general helper functions.
+ *
+ * Rob Simari
+ * Royce Branning
+ * Will Badart
+ *
+ * created: OCT 2017
+ **/
+
+#include "utils.h"
+
+// wrapper for write()
+int _write(int socket_fd, char* message, char error_msg[]) {
+    int bytes = write(socket_fd, message, strlen(message));
+	if (bytes == -1) {
+		error(error_msg);
+	}
+    return bytes;
+}
+
+int _write(int socket_fd, char* message, char error_msg[], int sz) {
+    int bytes = write(socket_fd, message, sz);
+    if (bytes == -1) {
+        error(error_msg);
+    }
+    return bytes;
+}
+
+// wrapper for read()
+int _read(int socket_fd, char* message, char error_msg[BUFSIZ]) {
+    int bytes = read(socket_fd, message, BUFSIZ);
+	if (bytes == -1) {
+		error(error_msg);
+	}
+    message[bytes] = '\0';
+    return bytes;
+}
+
+int _read(int socket_fd, char* message, char error_msg[BUFSIZ], int sz) {
+    int bytes = read(socket_fd, message, sz);
+    if (bytes == -1) {
+        error(error_msg);
+    }
+    message[bytes] = '\0';
+    return bytes;
+}
+
+void error(char *fmt, ...) {
+    va_list args;
+    char msg[BUFSIZ], *prefix = "ERROR: ";
+    strcpy(msg, prefix);
+
+    va_start(args, fmt);
+    vsprintf(msg+strlen(prefix), fmt, args);
+    va_end(args);
+
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+
+void log(char *fmt, ...) {
+    return;
+    //if(LOG_LVL < 1) return;
+    va_list args;
+    char msg[BUFSIZ], *prefix = "DEBUG: ";
+    strcpy(msg, prefix);
+
+    va_start(args, fmt);
+    vsprintf(msg+strlen(prefix), fmt, args);
+    va_end(args);
+
+    std::cerr << msg << std::endl;
+}
+
+
+std::string str2lower(std::string s) {
+    std::transform(
+        s.begin(), s.end(), s.begin(),
+        [](unsigned char c) { return std::tolower(c); }
+        );
+    return s;
+}
+
+std::string permissions_string(struct stat st) {
+    std::string perm_str;
+    perm_str.append( (S_ISDIR(st.st_mode)) ? "d" : "-");
+    perm_str.append( (st.st_mode & S_IRUSR) ? "r" : "-");
+    perm_str.append( (st.st_mode & S_IWUSR) ? "w" : "-");
+    perm_str.append( (st.st_mode & S_IXUSR) ? "x" : "-");
+    perm_str.append( (st.st_mode & S_IRGRP) ? "r" : "-");
+    perm_str.append( (st.st_mode & S_IWGRP) ? "w" : "-");
+    perm_str.append( (st.st_mode & S_IXGRP) ? "x" : "-");
+    perm_str.append( (st.st_mode & S_IROTH) ? "r" : "-");
+    perm_str.append( (st.st_mode & S_IWOTH) ? "w" : "-");
+    perm_str.append( (st.st_mode & S_IXOTH) ? "x" : "-");
+    
+    return perm_str;
+}
---
+++ server/utils.h
@@ -0,0 +1,69 @@
+/**
+ * src/utils.h
+ *
+ * Provides a number of general functions for myftpd.
+ *
+ * Rob Simari
+ * Royce Branning
+ * Will Badart
+ *
+ * created: OCT 2017
+ **/
+
+#pragma once
+
+#include <algorithm>    // transform
+#include <cctype>       // tolower
+#include <cstdlib>      // exit
+#include <cstdarg>      // va_list
+#include <cstring>      // strcmp
+#include <iostream>     // cout
+#include <stdio.h>      // fseek, fopen
+#include <sys/stat.h>   // stat
+#include <map>
+#include <string>
+#include <sys/socket.h> // socket, connect, send, recv
+#include <unistd.h>     // read, write
+
+#define streq(a, b) (strncmp(a, b, BUFSIZ)==0)
+
+// Default log level (i.e. disabled)
+extern int LOG_LVL;
+
+// Standardized command labels
+enum class Command {
+    DWLD, UPLD, DELF, LIST, MDIR, RDIR, CDIR, QUIT };
+
+// Standard command labels, prevent typos
+// usage: Command cmd = CMD_LABELS[cmdstr]
+const std::map<std::string, Command> CMD_LABELS = {
+    {"dwld", Command::DWLD},
+    {"upld", Command::UPLD},
+    {"delf", Command::DELF},
+    {"list", Command::LIST},
+    {"mdir", Command::MDIR},
+    {"rdir", Command::RDIR},
+    {"cdir", Command::CDIR},
+    {"quit", Command::QUIT},
+    {"exit", Command::QUIT},
+};
+
+// wrapper for write()
+int _write(int socket_fd, char* message, char* error_msg);
+int _write(int socket_fd, char* message, char* error_msg, int sz);
+
+// wrapper for read()
+int _read(int socket_fd, char* message, char* error_msg);
+int _read(int socket_fd, char* message, char* error_msg, int sz);
+
+// Report error and exit with status
+void error(char *fmt, ...);
+
+// Conditionally report debugging messages
+void log(char *fmt, ...);
+
+// Convert std::string to lowercase
+std::string str2lower(std::string s);
+
+// Return permissions string from stat struct
+std::string permissions_string(struct stat st);
